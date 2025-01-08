4f6627430fce850218b6e18747f021f08d90a734
Kiseok <kisunk4546@gmail.com>
2025-01-08 16:55:14 +0900

    fetch : calendar설정 및 페이지 로딩

--- frontend/pages/Home.tsx
+++ frontend/pages/Home.tsx
@@ -1,12 +1,54 @@
-import {Text, View} from "react-native";
+import {StyleSheet, Text, View} from "react-native";
+import {useState} from "react";
+import Calendar from "../components/Calendar.tsx";
 
 
 const Home = () => {
+    const today = new Date();
+    const formattedToday = today.toISOString().split('T')[0];
+    const [selectedDate, setSelectedDate] = useState(formattedToday);
+
+    const handler = () => {
+        console.log("button click");
+    }
+
     return (
-        <View>
-            <Text>dfdf</Text>
+        <View style={styles.container}>
+
+            <Calendar
+                current={formattedToday}
+                onDayPress={(day) => {
+                    setSelectedDate(day.dateString);
+                    console.log('selected Day : ', day);
+                }}
+                markedDates={{
+                    [selectedDate] : {
+                        selected: true,
+                        selectedColor: '#74AFD1',
+                    },
+                }}
+                theme={{
+                    todayTextColor: '#74AFD1',
+                    arrowColor: '#74AFD1',
+                    textDayFontWeight: "600",
+                }}
+                />
+            <Text style={styles.text}>Selected Date : {selectedDate}</Text>
         </View>
     )
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+    },
+    text:{
+        flex:1
+    }
+});
+
+
 
 export default Home;
