65d2038858fb39cfa739d07920b0a6937b7e5ade
Kiseok <kisunk4546@gmail.com>
2025-01-08 15:28:49 +0900

    fix : CORS setting

--- src/main/java/todo/sub/config/WebConfig.java
+++ src/main/java/todo/sub/config/WebConfig.java
@@ -1,7 +1,14 @@
 package todo.sub.config;
 
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:19006")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
 
+    }
 }
