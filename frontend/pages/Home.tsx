0b998dcce62822b6344ca0976bb8803f7540e63b
Kiseok <kisunk4546@gmail.com>
2025-01-16 00:29:57 +0900

    fetch : model 설정 및 button 추가

--- frontend/pages/Home.tsx
+++ frontend/pages/Home.tsx
@@ -1,8 +1,11 @@
 import {StyleSheet, Text, View} from "react-native";
-import {useContext, useState} from "react";
+import {useContext, useRef, useState} from "react";
 import Calendar from "../components/Calendar.tsx";
 import TodoList from "../components/TodoList.tsx";
 import {NavigationProp, useNavigation} from "@react-navigation/native";
+import {Modalize} from "react-native-modalize";
+import Button from "../components/Button.tsx";
+
 
 type StackParam = {
     Edit : { id: string };
@@ -32,14 +35,28 @@ const Home = () => {
     const today = new Date();
     const formattedToday = today.toISOString().split('T')[0];
     const [selectedDate, setSelectedDate] = useState(formattedToday);
+    const [selectedId, setSelectedId] = useState<bigint|null>(null);
     const navigation = useNavigation<NavigationProp<StackParam>>()
+    const modalizeRef = useRef<Modalize>(null)
 
-    const handleEdit = (id: bigint) => {
+    const openModal = () => {
+        modalizeRef.current?.open();
+    };
+    const closeModal = () => {
+        modalizeRef.current?.close();
+    }
+
+    const handleEdit = (id:bigint) => {
+        openModal()
+        setSelectedId(id);
+    };
+
+    const EditLoad = (id:bigint) => {
         console.log("수정 클릭:", id);
         const selectedTodo = Mock.find((item) => item.id === id);
         console.log("findItem :" , selectedTodo)
-        navigation.navigate("Edit", { id: id.toString() });
-    };
+        navigation.navigate("Edit" , {id:id.toString()})
+    }
 
     return (
         <View style={styles.container}>
@@ -72,10 +89,34 @@ const Home = () => {
                     onPressEdit={handleEdit}
                     onPressItem={handleItem}
                 />
+                <Modalize
+                    ref={modalizeRef}
+                    snapPoint={300}
+                    modalHeight={500}
+                    >
+                    <View style={styles.modalContent}>
+                        <View style={styles.gridContainer}>
+                            <View style={styles.buttonWrapper}>
+                                <Button type={"primary"} text={"수정하기"} onClick={() => { if (selectedId) EditLoad(selectedId) }} />
+                            </View>
+                            <View style={styles.buttonWrapper}>
+                                <Button type={"negative"} text={"삭제하기"} onClick={() => console.log("삭제")} />
+                            </View>
+                            <View style={styles.buttonWrapper}>
+                                <Button type={"finish"} text={"메모"} onClick={() => console.log("완료")} />
+                            </View>
+                            <View style={styles.buttonWrapper}>
+                                <Button type={"next"} text={"내일하기"} onClick={closeModal} />
+                            </View>
+                        </View>
+                    </View>
+                </Modalize>
             </View>
         </View>
     );
 }
+
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -92,6 +133,25 @@ const styles = StyleSheet.create({
         width: '100%', // 리스트 너비를 화면에 맞춤
         paddingHorizontal: 10, // 리스트 좌우 여백
         paddingTop: '105%' // 캘린더와 리스트 간 간격 확보
+    },
+    modalContent: {
+        flex: 1,
+        padding: 20,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    gridContainer: {
+        flexDirection: "row",
+        flexWrap: "wrap", // 행이 꽉 차면 다음 줄로 이동
+        justifyContent: "space-between", // 버튼 간격 조절
+        alignItems: "center", // 수직 정렬
+        width: "100%", // 가로 너비
+        paddingVertical: 30
+    },
+    buttonWrapper: {
+        width: "45%", // 버튼 너비 (2개씩 배치)
+        marginVertical: 10, // 버튼 상하 여백
+        height: "50%"
     },
 });
 
