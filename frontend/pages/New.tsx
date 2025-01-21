9224b5b485840b71cf2778ffa7653bd94d95fb91
Kiseok <kisunk4546@gmail.com>
2025-01-21 14:07:43 +0900

    fetch : new page 생성

--- frontend/pages/New.tsx
+++ frontend/pages/New.tsx
@@ -1,6 +1,8 @@
 import {NavigationProp, useNavigation} from "@react-navigation/native";
 import {StyleSheet, View, Text} from "react-native";
 import React from "react";
+import Header from "../components/Header.tsx";
+import Button from "../components/Button.tsx";
 
 type PageParam = {
     Home :undefined;
@@ -9,15 +11,25 @@ type PageParam = {
 
 const New = () => {
     const navigation = useNavigation<NavigationProp<PageParam>>()
+    const returnPage = () => {
+        console.log("EditPage 뒤로가기")
+        navigation.goBack();
+    }
 
+    const finish = () => {
+        console.log("EditPage 완료")
+    }
     return (
-        <View>
+        <View style={styles.container}>
+            <Header leftChild={<Button type="back" text={"<"} onClick={returnPage}/>}
+                    rightChild={<Button type="primary" text={"완료"} onClick={finish}/>}
+            />
             <Text>New page</Text>
         </View>
     );
 };
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
     container:{
         flex:1
     }
