0d0eae1f35a939983413b8623b5f3a402f51a0d3
Kiseok <kisunk4546@gmail.com>
2025-01-22 18:29:46 +0900

    fetch : new페이지 생성 및 COntext연결해 todo 추가 기능 생성

--- frontend/pages/New.tsx
+++ frontend/pages/New.tsx
@@ -1,8 +1,8 @@
 import {NavigationProp, useNavigation} from "@react-navigation/native";
-import {StyleSheet, View, Text} from "react-native";
-import React from "react";
+import {StyleSheet, View, Text, TextInput, Button} from "react-native";
+import React, {useLayoutEffect, useRef, useState} from "react";
 import Header from "../components/Header.tsx";
-import Button from "../components/Button.tsx";
+import {useTodoContext} from "../hooks/useTodoContext.tsx";
 
 type PageParam = {
     Home :undefined;
@@ -11,20 +11,47 @@ type PageParam = {
 
 const New = () => {
     const navigation = useNavigation<NavigationProp<PageParam>>()
-    const returnPage = () => {
-        console.log("EditPage 뒤로가기")
+    const {state, dispatch } = useTodoContext();
+    const [inputValue, setInputValue] = useState("");
+
+    const handleComplete = () =>{
+        if(!inputValue.trim()){
+            console.log("입력값이 없습니다.");
+            return;
+        }
+
+        const newTodo = {
+            id: BigInt(Date.now()),
+            //날짜는 선택할수있게 변경 예정
+            createDate: new Date().toISOString().split('T')[0],
+            content: inputValue,
+        };
+
+        console.log("new todo : " ,newTodo )
+        dispatch({type:"CREATE", payload: newTodo});
+
         navigation.goBack();
     }
 
-    const finish = () => {
-        console.log("EditPage 완료")
-    }
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerRight: () => (
+                <Button title="완료" onPress={handleComplete}/>
+            )
+        });
+    }, [navigation, handleComplete])
+
+
+
     return (
         <View style={styles.container}>
-            <Header leftChild={<Button type="back" text={"<"} onClick={returnPage}/>}
-                    rightChild={<Button type="primary" text={"완료"} onClick={finish}/>}
-            />
-            <Text>New page</Text>
+            <TextInput
+                style={styles.input}
+                placeholder="일정을 입력하세요"
+                value={inputValue}
+                onChangeText={setInputValue}
+                focusable={true}
+                />
         </View>
     );
 };
@@ -32,7 +59,13 @@ const New = () => {
 const styles = StyleSheet.create({
     container:{
         flex:1
+    },
+    input: {
+        height: 50,
+        borderBottomWidth: 1,
+        paddingHorizontal: 10,
     }
+
 })
 
 export default New;
